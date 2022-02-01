ec6214b7a08845f41f370f798873e704194b8f88
Santosh <santoshghalan@gmail.com>
2022-02-02 00:52:31 +1100

    commit tree 4127674790c4d979bb5711f39def16f87b04437f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(){
+    const innerFunction = function(empty = "special"){
+        return function(){
+let thisThing = `You are ${empty}`
+        }
+    }
+}
+
+
